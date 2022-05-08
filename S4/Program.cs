76862ba1a2f3bb4c0cd4dafa5c67b7e9f3bd54b2
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:21:08 +0430

    Add project files.

---
+++ S4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int Fn = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int Sn = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int Tn = int.Parse(Console.ReadLine());
+            int ave = (Fn + Sn + Tn) / 3;
+            Console.WriteLine("Average numders: " + ave);
+            Console.ReadKey();
+        }
+    }
+}
